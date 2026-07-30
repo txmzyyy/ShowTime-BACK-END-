ccd7a4df06bde8157118b159dacc3f8f9772c112
txmzyyy <tamaramk08@gmail.com>
2026-07-30 20:24:18 +0300

    add to_dict methods to all models

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from config import Config
 from extensions import db, migrate
-from models import Movie
-from flask import jsonify
+from models import Movie, Screening, Booking
+from flask import request, jsonify
 
 app= Flask(__name__)
 app.config.from_object(Config)
@@ -23,6 +23,25 @@ def home():
 def get_movies():
     movies = Movie.query.all()
     return jsonify([movie.to_dict()for movie in movies])
+
+@app.route("/movies/<int:id>")
+def get_movie(id):
+    movie = Movie.query.all()
+    return jsonify([movie.to_dict()])
+
+@app.route("/screenings")
+def get_screenings():
+    screenings = screenings.query.all()
+    return jsonify([screening.to_dict() for screening in screenings])
+
+@app.route("/bookings", methods=["POST"])
+def get_booking():
+    data = request.get_json()
+    booking = booking(user_name = data["user_name"],
+    screening_id=data["screening_id"], seats=data["seats"])
+    db.session.add(booking)
+    db.session.commit()
+    return jsonify(booking.to_dict()), 201
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -9,6 +9,15 @@ class User(db.Model):
     role = db.Column(db.String(20), default="customer")
     bookings = db.relationship("Booking", backref="user", lazy=True)
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "first name": self.first_name,
+            "last name": self.last_name,
+            "email": self.email,
+            "password": self.password,
+            "role": self.role,
+        }
 class Movie(db.Model):
     __tablename__ = "movies"
     id = db.Column(db.Integer, primary_key=True)
@@ -31,47 +40,69 @@ class Movie(db.Model):
 
 class Hall(db.Model):
     __tablename__ = "halls"
-
     id = db.Column(db.Integer, primary_key=True)
     hall_name = db.Column(db.String(50), nullable=False)
     screen_type = db.Column(db.String(50), nullable=False)
-
     screenings = db.relationship("Screening", backref="hall", lazy=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "hall name": self.hall_name,
+            "screen type": self.screen_type,
+        }
 
 class Screening(db.Model):
     __tablename__ = "screenings"
-
     id = db.Column(db.Integer, primary_key=True)
-
     movie_id = db.Column(db.Integer, db.ForeignKey("movies.id"))
     hall_id = db.Column(db.Integer, db.ForeignKey("halls.id"))
-
     date = db.Column(db.String(20))
     time = db.Column(db.String(20))
     available_seats = db.Column(db.Integer)
-
     bookings = db.relationship("Booking", backref="screening", lazy=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "movie_id": self.movie_id,
+            "hall_id": self.hall_id,
+            "date": self.date,
+            "time": self.time,
+            "available_seats": self.available_seats,
+        }
 
 class Booking(db.Model):
     __tablename__ = "bookings"
-
     id = db.Column(db.Integer, primary_key=True)
-
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     screening_id = db.Column(db.Integer, db.ForeignKey("screenings.id"))
-
     number_of_tickets = db.Column(db.Integer)
     booking_status = db.Column(db.String(30))
     booking_date = db.Column(db.String(30))
-
     tickets = db.relationship("Ticket", backref="booking", lazy=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user id": self.user_id,
+            "screening id": self.screening_id,
+            "number of tickets": self.number_of_tickets,
+            "booking status": self.booking_status,
+            "booking date": self.booking_date,
+        }
 
 class Ticket(db.Model):
     __tablename__ = "tickets"
-
     id = db.Column(db.Integer, primary_key=True)
-
     booking_id = db.Column(db.Integer, db.ForeignKey("bookings.id"))
     hall_id = db.Column(db.Integer, db.ForeignKey("halls.id"))
-
     ticket_status = db.Column(db.String(30))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "booking_id": self.booking_id,
+            "hall_id": self.hall_id,
+            "ticket_status": self.ticket_status,
+        }
