d0647084edca3065cffe72a4960637ff9a891201
txmzyyy <tamaramk08@gmail.com>
2026-07-30 19:23:28 +0300

    set up /movies endpoint

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from config import Config
 from extensions import db, migrate
+from models import Movie
+from flask import jsonify
 
 app= Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +18,11 @@ def home():
     return {
         "message": "Welcome to ShowTime"
     }
+
+@app.route("/movies", methods=["GET"])
+def get_movies():
+    movies = Movie.query.all()
+    return jsonify([movie.to_dict()for movie in movies])
 
 if __name__ == "__main__":
     app.run(debug=True)
